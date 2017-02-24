d7522461d3b9ee9fe71aeeeaaaebff24d476e6cd
curso17 <curso17java@gmail.com>
2017-02-24 14:04:25 +0100

    Anotaciones servicio GestorDirectoriosImpl

--- src/main/java/es/cic/curso/grupo6/ejercicio027/servicio/ServicioGestorDirectoriosImpl.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/servicio/ServicioGestorDirectoriosImpl.java
@@ -3,32 +3,36 @@ package es.cic.curso.grupo6.ejercicio027.servicio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.cic.curso.grupo6.ejercicio027.modelo.Directorio;
 import es.cic.curso.grupo6.ejercicio027.repositorio.RepositorioDirectorio;
 
+
+@Service
+@Transactional
 public class ServicioGestorDirectoriosImpl implements ServicioGestorDirectorios {
-	
+
 	private static final String ERROR_DIRECTORIO_ID = "No existe ningún directorio en BB.DD. con ese ID";
 	private static final String RUTA_SECUNDARIA = "/prueba/directorio";
 
 	@Autowired
 	private RepositorioDirectorio repositorioDirectorio;
-	
+
 	@Autowired
 	private ServicioGestorFicheros servicioGestorFicheros;
-	
+
 	@Override
 	public void agregaDirectorio(Directorio directorio) {
-		
+
 		repositorioDirectorio.create(directorio);
-		
-	
+
 	}
 
 	@Override
 	public Directorio obtenDirectorio(Long id) {
-		
+
 		Directorio directorio = repositorioDirectorio.read(id);
 		if (directorio == null) {
 			throw new IllegalArgumentException(ERROR_DIRECTORIO_ID + ": " + id);
@@ -36,33 +40,31 @@ public class ServicioGestorDirectoriosImpl implements ServicioGestorDirectorios 
 		return directorio;
 	}
 
-
-
 	@Override
 	public Directorio modificaDirectorio(Long id, Directorio directorio) {
 		Directorio directorioInicial = obtenDirectorio(id);
-		
+
 		String rutaNueva = directorioInicial.getRuta();
 		if (!servicioGestorFicheros.listaFicherosPorDirectorio(id).isEmpty()) {
 			throw new IllegalArgumentException("ERROR BUSCANDO DIRECTORIOS");
 		}
-		
+
 		directorioInicial.setRuta(RUTA_SECUNDARIA);
 		repositorioDirectorio.update(directorioInicial);
 		return directorioInicial;
 	}
-	
+
 	@Override
 	public Directorio eliminarDirectorio(Long id) {
-		
+
 		Directorio directorio = repositorioDirectorio.delete(id);
-		
+
 		return directorio;
 	}
 
 	@Override
 	public List<Directorio> listaEntradasPorDirectorio(Long id) {
-		
+
 		return repositorioDirectorio.list();
 	}
 
