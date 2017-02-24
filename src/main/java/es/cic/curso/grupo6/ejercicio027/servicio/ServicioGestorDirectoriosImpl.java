bb4c3e407e3c216095891d041ac815eac561bb5a
curso06 <curso06java@gmail.com>
2017-02-24 12:28:08 +0100

    comenzando serviciogestordirectoriosimpl

--- src/main/java/es/cic/curso/grupo6/ejercicio027/servicio/ServicioGestorDirectoriosImpl.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/servicio/ServicioGestorDirectoriosImpl.java
@@ -1,5 +1,39 @@
 package es.cic.curso.grupo6.ejercicio027.servicio;
 
-public class ServicioGestorDirectoriosImpl {
+import java.util.List;
+
+import es.cic.curso.grupo6.ejercicio027.modelo.Directorio;
+
+public class ServicioGestorDirectoriosImpl implements ServicioGestorDirectorios {
+
+	@Override
+	public void agregaDirectorio(Directorio directorio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Directorio obtenEntradaDirectorio(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Directorio eliminarDirectorio(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Directorio modificaDirectirio(Long id, Directorio directorio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Directorio> listaEntradasPorDirectorio(Long idFichero) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
