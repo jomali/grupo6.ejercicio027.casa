54ad58e9c1e2944886d799203afec676deaf889b
curso17 <curso17java@gmail.com>
2017-02-24 11:31:59 +0100

    Correcciones sobre Fichero

--- src/main/java/es/cic/curso/grupo6/ejercicio027/modelo/Fichero.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/modelo/Fichero.java
@@ -1,14 +1,13 @@
 package es.cic.curso.grupo6.ejercicio027.modelo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,48 +19,23 @@ public class Fichero implements Identificable<Long> {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	/** Referencia al directorio del que cuelga el fichero. */
+	@JoinColumn(name = "id_directorio")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Directorio directorio;
 
+	/** Nombre del fichero. */
 	@Column(name = "nombre")
 	private String nombre;
 
+	/** Descripción <em>opcional</em>. */
 	@Column(name = "descripcion")
 	private String descripcion;
 
+	/** Código con la versión del fichero. */
 	@Column(name = "version")
-	private String version;
-
-	@OneToMany(mappedBy = "directorio")
-	private List<Fichero> directorios = new ArrayList<>();
-
-	public Fichero() {
-		super();
-
-	}
-
-	public Fichero(String nombre, String descripcion, String version, List<Fichero> directorios) {
-		super();
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.version = version;
-		this.directorios = directorios;
-	}
-
-	public Fichero(Long id, String nombre, String descripcion, String version, List<Fichero> directorios) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.version = version;
-		this.directorios = directorios;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+	private Double version;
 
 	/**
 	 * @return the id
@@ -72,44 +46,75 @@ public class Fichero implements Identificable<Long> {
 	}
 
 	/**
-	 * @param id
-	 *            the id to set
+	 * @return the directorio
+	 */
+	public Directorio getDirectorio() {
+		return directorio;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	/**
+	 * @return the version
+	 */
+	public Double getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param id the id to set
 	 */
 	@Override
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getNombre() {
-		return nombre;
+	/**
+	 * @param directorio the directorio to set
+	 */
+	public void setDirectorio(Directorio directorio) {
+		this.directorio = directorio;
 	}
 
+	/**
+	 * @param nombre the nombre to set
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	public String getDescripcion() {
-		return descripcion;
-	}
-
+	/**
+	 * @param descripcion the descripcion to set
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(String version) {
+	/**
+	 * @param version the version to set
+	 */
+	public void setVersion(Double version) {
 		this.version = version;
 	}
 
-	public List<Fichero> getDirectorios() {
-		return directorios;
-	}
-
-	public void setDirectorios(List<Fichero> directorios) {
-		this.directorios = directorios;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -131,8 +136,8 @@ public class Fichero implements Identificable<Long> {
 
 	@Override
 	public String toString() {
-		return "Directorio [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", version=" + version
-				+ ", directorios=" + directorios + "]";
+		return "Fichero [id=" + id + ", directorioId=" + directorio.getId() + ", nombre=" + nombre + ", descripcion="
+				+ descripcion + ", version=" + version + "]";
 	}
 
 }
