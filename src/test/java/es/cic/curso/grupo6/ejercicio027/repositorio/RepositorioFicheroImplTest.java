81ba7b50d72140d26e5097e44c1affc36dfa297d
curso06 <curso06java@gmail.com>
2017-02-24 11:22:27 +0100

    repositorio fichero

---
+++ src/test/java/es/cic/curso/grupo6/ejercicio027/repositorio/RepositorioFicheroImplTest.java
@@ -0,0 +1,49 @@
+package es.cic.curso.grupo6.ejercicio027.repositorio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RepositorioFicheroImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testObtenClaseT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteK() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testList() {
+		fail("Not yet implemented");
+	}
+
+}
