ea0722ad6ac872af741ab6669447846248fc2459
curso06 <curso06java@gmail.com>
2017-02-24 11:42:16 +0100

    repositorioficheroimpltest

--- src/test/java/es/cic/curso/grupo6/ejercicio027/repositorio/RepositorioFicheroImplTest.java
+++ src/test/java/es/cic/curso/grupo6/ejercicio027/repositorio/RepositorioFicheroImplTest.java
@@ -1,49 +1,130 @@
 package es.cic.curso.grupo6.ejercicio027.repositorio;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import org.junit.Before;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
+import es.cic.curso.grupo6.ejercicio027.modelo.Directorio;
+import es.cic.curso.grupo6.ejercicio027.modelo.Fichero;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:es/cic/curso/grupo6/ejercicio027/applicationContext.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
+@Transactional
 public class RepositorioFicheroImplTest {
+public static final int NUMERO_ELEMENTOS_PRUEBA = 10;
+	
+	@Autowired
+	private RepositorioFichero sut;
+	
+	@PersistenceContext
+	protected EntityManager em;
+	
+	
 
-	@Before
-	public void setUp() throws Exception {
-	}
+	private Fichero generaFichero() {
+		
+		Directorio directorio = new Directorio();
+		directorio.setRuta("/directorio/prueba/");
+		em.persist(directorio);
+		em.flush();
+		
+		Fichero elemento = new Fichero();
+		elemento.setNombre("Inicial");
+		elemento.setDescripcion("Elemento inicial de prueba");
+		elemento.setVersion(2.0);
+		elemento.setDirectorio(directorio);
 
-	@Test
-	public void testObtenClaseT() {
-		fail("Not yet implemented");
+		sut.create(elemento);
+		return elemento;
+
 	}
 
 	@Test
 	public void testCreate() {
-		fail("Not yet implemented");
+		Fichero elemento;
+		
+		elemento = new Fichero();
+		assertNull(elemento.getId());
+
+		elemento = generaFichero();
+		assertNotNull(elemento.getId());
 	}
 
 	@Test
 	public void testRead() {
-		fail("Not yet implemented");
+		Fichero elemento1 = generaFichero();
+		Fichero elemento2 = sut.read(elemento1.getId());
+
+		assertTrue(elemento1.getId().equals(elemento2.getId()));
+		assertTrue(elemento1.getNombre().equals(elemento2.getNombre()));
+		assertEquals(elemento1.getDescripcion(), elemento2.getDescripcion(), 0.0001);
+		assertEquals(elemento1.getVersion(), elemento2.getVersion(), 0.0001);
+
+		try {
+			@SuppressWarnings("unused")
+			Fichero elemento3 = sut.read(Long.MIN_VALUE);
+			fail("No deberían existir elementos con el ID pasado");
+		} catch (PersistenceException pe) {
+
+		}
 	}
 
 	@Test
 	public void testUpdate() {
-		fail("Not yet implemented");
+		Fichero original = generaFichero();
+		Fichero clon = new Fichero();
+		clon.setId(original.getId());
+		clon.setNombre(original.getNombre());
+		clon.setDescripcion(original.getDescripcion());
+		clon.setVersion(original.getVersion());
+		clon.setDirectorio(original.getDirectorio());
+
+		original.setNombre("No Principal");
+		original.setDescripcion("Caso de prueba");
+		sut.update(original);
+
+		Fichero modificado = sut.read(original.getId());
+		assertEquals(original.getNombre(), modificado.getNombre());
+		assertNotEquals(clon.getDescripcion(), modificado.getDescripcion());
 	}
 
 	@Test
-	public void testDeleteK() {
-		fail("Not yet implemented");
-	}
+	public void testDelete() {
+		Fichero elemento = generaFichero();
+		sut.delete(elemento.getId());
 
-	@Test
-	public void testDeleteT() {
-		fail("Not yet implemented");
+		Fichero resultado = sut.read(elemento.getId());
+		assertNull(resultado);
 	}
 
 	@Test
 	public void testList() {
-		fail("Not yet implemented");
+		for (int i = 0; i < NUMERO_ELEMENTOS_PRUEBA; i++) {
+			generaFichero();
+		}
+
+		List<Fichero> lista = sut.list();
+		assertEquals(NUMERO_ELEMENTOS_PRUEBA, lista.size());
 	}
 
 }
