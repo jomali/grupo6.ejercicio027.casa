9d2ee031c66d8a99248d4760a56e38eb4712958e
cursojava <cursojava@BSVM-CURSOJAVA>
2017-02-25 11:58:06 +0100

    Bocetos de VistaFormularios

--- src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaFormFile.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaFormFile.java
@@ -1,5 +1,71 @@
 package es.cic.curso.grupo6.ejercicio027.vista;
 
-public class VistaFormFile {
+import com.vaadin.data.fieldgroup.PropertyId;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
+import es.cic.curso.grupo6.ejercicio027.modelo.Fichero;
+
+public class VistaFormFile extends FormLayout{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1842474819854513394L;
+
+	@PropertyId("nombre")
+	protected TextField nombre;
+	@PropertyId("descripcion")
+	protected TextField descripcion;
+	@PropertyId("version")
+	protected TextField version;
+	
+	private Button aceptar;
+	private Button cancelar;
+	private Fichero fichero;
+	
+	private VistaFiles padre;
+	
+	public VistaFormFile(VistaFiles padre) {
+		this.padre = padre;
+		
+		VerticalLayout verticalPrincipal= new VerticalLayout();
+
+		HorizontalLayout nombreLayout = new HorizontalLayout();
+		nombre = new TextField("File Name: ");
+		nombre.setInputPrompt("File Name:");
+		nombreLayout.addComponent(nombre);
+		HorizontalLayout descripcionLayout = new HorizontalLayout();
+		descripcion = new TextField("Description: ");
+		descripcion.setInputPrompt("Description");
+		descripcionLayout.addComponent(descripcion);
+		HorizontalLayout versionLayout = new HorizontalLayout();
+		version = new TextField("Version: ");
+		version.setInputPrompt("Version");
+		versionLayout.addComponent(version);
+
+		HorizontalLayout buttonsLayout = new HorizontalLayout();
+		aceptar = new Button("Ok");
+		aceptar.setIcon(FontAwesome.SAVE);
+		aceptar.addClickListener(e -> {
+			//padre.cargaGrid(fichero);
+			//TODO Aqui tenemos que hacer que vaya a VistaFile
+		});
+		cancelar = new Button("Cancel");
+		cancelar.setIcon(FontAwesome.CLOSE);
+		cancelar.addClickListener(e-> {
+			//padre.borrarGrid(fichero);
+			//TODO Aqui tenemos que hacer que vaya a VisaHome
+		});
+		buttonsLayout.addComponents(aceptar, cancelar);
+		
+		addComponents(verticalPrincipal);
+		
+		
+		//setFichero(null);
+	}
 }
--- src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaFormFolder.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaFormFolder.java
@@ -1,5 +1,65 @@
 package es.cic.curso.grupo6.ejercicio027.vista;
 
-public class VistaFormFolder {
+import com.vaadin.data.fieldgroup.PropertyId;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
+import es.cic.curso.grupo6.ejercicio027.modelo.Directorio;
+import es.cic.curso.grupo6.ejercicio027.modelo.Fichero;
+
+public class VistaFormFolder extends FormLayout{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5081842639219654799L;
+
+	@PropertyId("ruta")
+	protected TextField ruta;
+	@PropertyId("descripcion")
+	protected TextField descripcion;
+	@PropertyId("version")
+	protected TextField version;
+	
+	private Button aceptar;
+	private Button cancelar;
+	private Directorio directorio;
+	
+	private VistaFolder padre;
+	
+	public VistaFormFolder(VistaFolder padre) {
+		this.padre = padre;
+		
+		VerticalLayout verticalPrincipal= new VerticalLayout();
+
+		HorizontalLayout nombreLayout = new HorizontalLayout();
+		ruta = new TextField("Route: ");
+		ruta.setInputPrompt("Route:");
+		nombreLayout.addComponent(ruta);
+
+
+		HorizontalLayout buttonsLayout = new HorizontalLayout();
+		aceptar = new Button("Ok");
+		aceptar.setIcon(FontAwesome.SAVE);
+		aceptar.addClickListener(e -> {
+			//padre.cargaGrid(fichero);
+			//TODO Aqui tenemos que hacer que vaya a VistaFile
+		});
+		cancelar = new Button("Cancel");
+		cancelar.setIcon(FontAwesome.CLOSE);
+		cancelar.addClickListener(e-> {
+			//padre.borrarGrid(fichero);
+			//TODO Aqui tenemos que hacer que vaya a VisaHome
+		});
+		buttonsLayout.addComponents(aceptar, cancelar);
+		
+		addComponents(verticalPrincipal);
+		
+		
+		//setFichero(null);
+	}
 }
