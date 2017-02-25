30d45d0d3ba3e73ba99da777b62736742d2a3f46
cursojava <cursojava@BSVM-CURSOJAVA>
2017-02-25 11:31:08 +0100

    cosillas

--- src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaHome.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaHome.java
@@ -3,7 +3,6 @@ package es.cic.curso.grupo6.ejercicio027.vista;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
@@ -12,11 +11,13 @@ public class VistaHome extends VerticalLayout implements View {
  		public VistaHome(Navigator navegador) {
 
 		VistaMenu vista = new VistaMenu();
-		HorizontalLayout layout = new HorizontalLayout();
+		VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
 		layout.setSpacing(true);
 		Label label = new Label("Prueba de Evaluación del Grupo 6");
-		layout.addComponent(label);
+		Label label2 = new Label("Curso06 - Jose María Cagigas");
+		Label label3 = new Label("Curso17 - Jose Francisco Martín");
+		layout.addComponents(label, label2, label3);
 	
 		addComponents(vista,layout);
 	
