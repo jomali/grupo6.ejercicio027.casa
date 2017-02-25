d3fa55d39beb5f9fd576ddfb276f2ca62f15bdf6
cursojava <cursojava@BSVM-CURSOJAVA>
2017-02-25 18:17:46 +0100

    haciendo combobox para directorios

--- src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaFicheros.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio027/vista/VistaFicheros.java
@@ -1,5 +1,10 @@
 package es.cic.curso.grupo6.ejercicio027.vista;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.web.context.ContextLoader;
+
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
@@ -13,6 +18,10 @@ import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.VerticalLayout;
 
+
+import es.cic.curso.grupo6.ejercicio027.modelo.Directorio;
+import es.cic.curso.grupo6.ejercicio027.modelo.Fichero;
+import es.cic.curso.grupo6.ejercicio027.servicio.ServicioGestorDirectorios;
 import es.cic.curso.grupo6.ejercicio027.servicio.ServicioGestorFicheros;
 
 public class VistaFicheros extends CustomComponent implements View {
@@ -21,24 +30,36 @@ public class VistaFicheros extends CustomComponent implements View {
 	private VerticalLayout layout;
 	private VerticalLayout cuerpo;
 	private ServicioGestorFicheros servicioGestorFicheros;
+	private ServicioGestorDirectorios servicioGestorDirectorios;
+	private ComboBox lista;
 	Button carga = new Button("Carga");
 	Button crea = new Button("Crea");
 	Button borra = new Button("Borra");
 	Button actualiza = new Button("Actualiza");
-	ComboBox escogeCarpeta = new ComboBox();
+	private List<Fichero> listaFicheros;
+	private List<Fichero> listaFicherosPorDirectorio;
+	private List<Directorio> listaDirectorios;
+	private static final String SELECCIONA = "Selecciona";
 	
 	MenuNavegacion vistaMenu = new MenuNavegacion();
 
 	
 	public VistaFicheros(Navigator navegador) {
 		super();
+		
+		servicioGestorFicheros = ContextLoader.getCurrentWebApplicationContext().getBean(ServicioGestorFicheros.class);
+		servicioGestorDirectorios = ContextLoader.getCurrentWebApplicationContext().getBean(ServicioGestorDirectorios.class);
+
+		listaDirectorios = servicioGestorDirectorios.listaDirectorios();
+		listaFicheros = servicioGestorFicheros.listaFicheros();
+
 		layout = new VerticalLayout();
 		layout.setMargin(true);
 		layout.setSpacing(true);
 		Grid gridArchivos = new Grid();
 		// GRID DE SESIONES:
 		
-		gridArchivos.setColumns("Ruta del Archivo", "Nombre del Archivo", "Descripción", "Versión");
+		gridArchivos.setColumns("Directorio", "Archivo", "Descripción", "Versión");
 		gridArchivos.setSizeFull();
 		gridArchivos.setSelectionMode(SelectionMode.SINGLE);
 		layout.addComponent(gridArchivos);
@@ -46,9 +67,22 @@ public class VistaFicheros extends CustomComponent implements View {
 		HorizontalLayout layoutHorizontal = new HorizontalLayout();
 		layoutHorizontal.setSpacing(true);
 		
+		
+		carga.setVisible(false);
 		crea.setVisible(false);
 		borra.setVisible(false);
 		actualiza.setVisible(false);
+		
+		lista = new ComboBox();
+		lista.setInputPrompt(SELECCIONA);
+		listaDirectorios = servicioGestorDirectorios.listaDirectorios();
+		for (int i = 1; i <= listaDirectorios.size(); i++) {
+			lista.addItem(i);
+			lista.setItemCaption(i, "Directorio " + i);
+		}
+
+		lista.setNullSelectionAllowed(false);
+		lista.setImmediate(true);
 		
 		carga.addClickListener(clickEvent -> {
 //			List<Fichero> listaFicheros = servicioGestorFicheros.listaFicheros();
@@ -56,8 +90,7 @@ public class VistaFicheros extends CustomComponent implements View {
 //				grillafiles.addRow(listaFicheros.get(i).getNombre(), listaFicheros.get(i).getDescripcion(), listaFicheros.get(i).getVersion());
 //			}
 			carga.setVisible(false);
-			escogeCarpeta.setVisible(true);
-			escogeCarpeta.addValueChangeListener(escogeCarpeta);
+
 			
 		});
 //		crea.setVisible(true);
